fe78be692ec38343fc7e369708686123127bc1c0
meganathan006 <mp9597000@gmail.com>
2025-02-13 15:08:50 +0530

    DOne

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+function clearOne() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1); // Removes the last character
+}
+
 function calculateResult() {
     try {
         document.getElementById('display').value = eval(document.getElementById('display').value);
